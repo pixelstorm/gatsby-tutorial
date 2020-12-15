75e7791e449a46e7d802d9faa055eeced09f6acf
daniel florido <dan@pixelstorm.com.au>
2020-12-16 09:02:09 +1100

    build basic pages - output content

---
+++ src/pages/blog.js
@@ -0,0 +1,13 @@
+import React from "react"
+import { Link } from "gatsby"
+
+const blog = () => {
+  return (
+    <div>
+      <h1>this is the blog</h1>
+      <Link to="/">Home Page</Link>
+    </div>
+  )
+}
+
+export default blog
