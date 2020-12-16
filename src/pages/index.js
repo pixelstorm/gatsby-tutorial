810024b202957fe20eed5af60fec25038c75b198
daniel florido <dan@pixelstorm.com.au>
2020-12-17 07:31:01 +1100

    add styled component example button

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,10 @@
 import React from "react"
 import Layout from "../components/layout"
+import { ExampleButton } from "../components/button"
 
 export default () => (
   <Layout>
     <h1>hello from gatsby</h1>
+    <ExampleButton>Example Button using styled components</ExampleButton>
   </Layout>
 )
